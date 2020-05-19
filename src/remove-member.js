eb5a7ab452db8d581a5bdada6fc96a3e6a8ee029
vishtree <vishal@getstream.io>
2020-05-19 11:07:11 +0200

    Fixing delete-channel and remove-member

--- src/remove-member.js
+++ src/remove-member.js
@@ -8,7 +8,6 @@ const client = new StreamChat(API_KEY, SECRET)
 const [,, ...args] = process.argv;
 const channelId = args[0];
 const memberToBeRemoved = args[1];
-const client = new StreamChat('q95x9hkbyd6p', 'bp5m2eqxybrhn89ausrzmjatg56v67rd74pstmdppsy2jxp7x4s4eqn4chnq83bg')
 
 const channel = client.channel('messaging', channelId);
 
