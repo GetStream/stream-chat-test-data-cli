e09ccd67d7b1700c5d8abc18dd85bbdc1db9d5f7
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2020-05-22 09:56:55 +0200

    add show-channel

---
+++ src/show-channel.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const {StreamChat} = require('stream-chat');
+const {config} = require('../config');
+const API_KEY = config.API_KEY;
+const SECRET = config.SECRET;
+const BASE_URL = config.BASE_URL;
+const client = new StreamChat(API_KEY, SECRET)
+if (BASE_URL) {
+    client.setBaseURL(BASE_URL);
+}
+
+const [,, ...args] = process.argv;
+const channelId = args[0];
+const userId = args[1];
+
+const channel = client.channel('messaging', channelId);
+
+channel.show(userId).then(response => {
+    console.log(response);
+    process.exit();
+})
